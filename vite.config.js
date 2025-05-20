204ebb984bdce7ef3c906f10b660b49a02119833
WaNNTaD <julien.deprez10@gmail.com>
2025-05-20 11:03:44 +0200

    Ajout de tous les fichiers du projet

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/freefor-you/', // Nom du dépôt GitHub
+  base: '/freeforyou-v2/', // Nom du dépôt GitHub
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
